dbea808732fe218c1c8a89021590655b6a2949ae
Luciano Vazquez <vazquezluciano5@gmail.com>
2022-09-29 16:17:41 -0300

    I added the function to add notes, save them into local storage and once added the note the popup closes itself

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const addBox =  document.querySelector(".add-box"),
 popupBox = document.querySelector(".popup-box"),
 closeIcon = popupBox.querySelector(".closeIcon"),
+titleTag = popupBox.querySelector("input"),
+descTag = popupBox.querySelector("textarea"),
 addBtn = popupBox.querySelector("button");
+
+
+const months = ["January","February","March","April","June","July","August","September","October","NovemberRain","December"];
+const notes = JSON.parse(localStorage.getItem("notes") || "[]")
 
 addBox.addEventListener("click",()=>{
     popupBox.classList.add("show");
@@ -11,6 +17,28 @@ closeIcon.addEventListener("click",()=>{
     popupBox.classList.remove("show");
 })
 
-addBtn.addEventListener("click",()=>{
-    console.log("button clicked")
+addBtn.addEventListener("click", e =>{
+    e.preventDefault();
+    let noteTitle = titleTag.value,
+    noteDesc = descTag.value;
+    
+    if(noteTitle || noteDesc){
+        let dateObj = new Date(),
+        month = months[dateObj.getMonth()-1],
+        day = dateObj.getDate(),
+        year = dateObj.getFullYear();
+
+
+        let noteInfo = {
+            title: noteTitle,description: noteDesc,
+            date:`${month} ${day}, ${year}`
+        }
+
+        
+        notes.push(noteInfo);
+
+        
+        localStorage.setItem("notes",JSON.stringify(notes));
+        closeIcon.click();
+    }
 })
