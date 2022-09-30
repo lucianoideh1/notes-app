d90e344e0d276ceb031dcbb822da5ca950061a15
Luciano Vazquez <vazquezluciano5@gmail.com>
2022-09-30 15:23:29 -0300

    now when you are about to delete a note you are asked to confirm

--- script.js
+++ script.js
@@ -66,6 +66,8 @@ function showMenu(elem){
 }
 
 function deleteNote(noteId){
+    let confirmDel = confirm("Are you sure you want to delete this note?");
+    if(!confirmDel) return;
     notes.splice(noteId, 1);
     localStorage.setItem("notes",JSON.stringify(notes));
     showNotes();
